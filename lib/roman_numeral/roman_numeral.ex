7e3d702bffe0cc48aa7ad6db1daafbb462d513b9
Patryk Woziński <patryk.wozinski@gmail.com>
2019-11-23 17:34:38 +0100

    kata done

--- lib/roman_numeral/roman_numeral.ex
+++ lib/roman_numeral/roman_numeral.ex
@@ -5,14 +5,36 @@ defmodule Kata.RomanNumeral do
     https://katalyst.codurance.com/roman-numerals
   """
 
+  @numeral_mapping [
+    {1000, "M"},
+    {900, "CM"},
+    {500, "D"},
+    {400, "CD"},
+    {100, "C"},
+    {90, "XC"},
+    {50, "L"},
+    {40, "XL"},
+    {10, "X"},
+    {9, "IX"},
+    {5, "V"},
+    {4, "IV"},
+    {1, "I"},
+  ]
+
   @doc """
     Converts arabic numerals to roman
   """
-  def romanFor(number) when is_integer(number) do
-    cond do
-      number == 1 -> "I"
-      number == 2 -> "II"
-      number == 3 -> "III"
-    end
+  def toRoman(number) when is_integer(number) do
+    toRoman(number, @numeral_mapping)
+  end
+
+  defp toRoman(0, _numerals), do: ""
+
+  defp toRoman(number, [{arabic, roman} | tail]) when number >= arabic do
+    roman <> toRoman(number - arabic, [{arabic, roman} | tail])
+  end
+
+  defp toRoman(number, [{arabic, roman} | tail]) when number < arabic do
+    toRoman(number, tail)
   end
 end
--- test/roman_numeral/roman_numeral_test.exs
+++ test/roman_numeral/roman_numeral_test.exs
@@ -6,20 +6,18 @@ defmodule KataTest.RomanNumeralTest do
       {1, "I"},
       {2, "II"},
       {3, "III"},
-#      {4, "IV"},
-#      {5, "V"},
-#      {9, "IX"},
-#      {15, "XV"},
-#      {40, "XL"},
-#      {50, "L"},
-#      {90, "XC"},
-#      {100, "C"},
-#      {400, "CD"},
-#      {500, "D"},
-#      {900, "CM"},
-#      {2019, "MMXIX"},
+      {4, "IV"},
+      {5, "V"},
+      {9, "IX"},
+      {15, "XV"},
+      {40, "XL"},
+      {50, "L"},
+      {901, "CMI"},
+      {1900, "MCM"},
+      {2019, "MMXIX"},
+      {3001, "MMMI"},
     ]
 
-    Enum.each(numbers, fn ({arabic, roman}) -> assert Kata.RomanNumeral.romanFor(arabic) == roman end)
+    Enum.each(numbers, fn ({arabic, roman}) -> assert Kata.RomanNumeral.toRoman(arabic) == roman end)
   end
 end
