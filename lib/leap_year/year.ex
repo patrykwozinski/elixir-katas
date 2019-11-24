12769cb48d089c7019dd202abaee7fee4077740e
Patryk Woziński <patryk.wozinski@gmail.com>
2019-11-24 14:03:09 +0100

    first step

--- lib/leap_year/year.ex
+++ lib/leap_year/year.ex
@@ -4,4 +4,15 @@ defmodule Kata.Year do
 
     https://katalyst.codurance.com/leap-year
   """
+
+  @doc """
+    Checks if given year is leap.
+
+    ## Examples
+    iex> Kata.Year.leap?(2000)
+    true
+  """
+  def leap?(year) do
+    rem(year, 4) == 0
+  end
 end
--- test/leapyear/year_test.exs
+++ test/leapyear/year_test.exs
@@ -2,4 +2,7 @@ defmodule KataTest.YearTest do
   use ExUnit.Case
   doctest Kata.Year
 
+  test "A year is not a leap year if not divisible by 4" do
+    assert Kata.Year.leap?(1980) == true
+  end
 end
