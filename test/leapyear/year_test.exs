20972992dfaaa52ea8a2bc4e409c91c094fe864f
Patryk Woziński <patryk.wozinski@gmail.com>
2019-11-24 14:06:33 +0100

    red

--- test/leapyear/year_test.exs
+++ test/leapyear/year_test.exs
@@ -3,10 +3,18 @@ defmodule KataTest.YearTest do
   doctest Kata.Year
 
   test "A year is not a leap year if not divisible by 4" do
-    assert Kata.Year.leap?(1995) == false
+    assert Kata.Year.leap?(1997) == false
   end
 
   test "A year is a leap year if divisible by 4" do
-    assert Kata.Year.leap?(1980) == true
+    assert Kata.Year.leap?(1996) == true
+  end
+
+  test "A year is a leap year if divisible by 400" do
+    assert Kata.Year.leap?(1600) == true
+  end
+
+  test "A year is not a leap year if divisible by 100 but not by 400" do
+    assert Kata.Year.leap?(1800) == false
   end
 end
