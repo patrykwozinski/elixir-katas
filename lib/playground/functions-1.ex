5dd9c3e82b4f9d92cf517b15dbc367888a74f650
Patryk Woziński <patryk.wozinski@gmail.com>
2020-08-20 21:44:41 +0200

    cleanup

--- lib/playground/functions-1.ex
+++ lib/playground/functions-1.ex
@@ -1,8 +1,8 @@
 list_concat = fn a, b -> a ++ b end
-IO.inspect list_concat.([:a, :b], [:c, :d])
+IO.inspect(list_concat.([:a, :b], [:c, :d]))
 
 sum = fn a, b, c -> a + b + c end
-IO.inspect sum.(1, 2, 3)
+IO.inspect(sum.(1, 2, 3))
 
 pair_tuple_to_list = fn a -> Tuple.to_list(a) end
-IO.inspect pair_tuple_to_list.({1234, 5678})
+IO.inspect(pair_tuple_to_list.({1234, 5678}))
