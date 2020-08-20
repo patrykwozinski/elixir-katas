927f160970ca4987421267811f38c597ab64b8e8
Patryk Woziński <patryk.wozinski@gmail.com>
2020-08-20 19:31:26 +0200

    third

--- lib/playground/exercise-functions-1.ex
+++ lib/playground/exercise-functions-1.ex
@@ -3,3 +3,6 @@ IO.inspect list_concat.([:a, :b], [:c, :d])
 
 sum = fn a, b, c -> a + b + c end
 IO.inspect sum.(1, 2, 3)
+
+pair_tuple_to_list = fn a -> Tuple.to_list(a) end
+IO.inspect pair_tuple_to_list.({1234, 5678})
